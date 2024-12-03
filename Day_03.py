12b3fcb7469c0115ac084f81b15b2f207ad27671
KodingForKittehs <grimtalon@gmail.com>
2024-12-03 08:02:51 -0500

    cleanup day3

--- Day_03.py
+++ Day_03.py
@@ -12,16 +12,15 @@ def clean_input(line):
     line = re.sub(r"don't\(\).*", "xx", line, re.DOTALL)
     return line
 
+def get_mult(line):
+    matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", line)
+    return sum(int(match[0]) * int(match[1]) for match in matches)
+
 def solve():
     line = get_input("input")
-
-    matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", line)
-    res1 = sum(int(match[0]) * int(match[1]) for match in matches)
-
+    res1 = get_mult(line)
     line = clean_input(line)
-    matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", line)
-
-    res2 = sum(int(match[0]) * int(match[1]) for match in matches)
+    res2 = get_mult(line)
     print(f"P1: {res1} P2: {res2}")
 
 solve()
