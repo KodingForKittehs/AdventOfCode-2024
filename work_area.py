81dc64fa9934625b0c9b4ac15e5f498eb27c4019
KodingForKittehs <grimtalon@gmail.com>
2024-12-05 23:07:57 -0500

    scaffolding

---
+++ work_area.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring, missing-final-newline, invalid-name, trailing-newlines, wrong-import-position, unused-import
+import importlib
+import sys
+import numpy as np
+import shapely
+sys.path.append("./")
+import kittehs_funkollection as kf
+importlib.reload(np)
+importlib.reload(shapely)
+importlib.reload(kf)
+
+
+
