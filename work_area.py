b8f4f052dbd12eb3d08adf11d86825999eb111a4
KodingForKittehs <grimtalon@gmail.com>
2024-12-05 23:55:51 -0500

    Add some Jupyter stuff for plots

--- work_area.py
+++ work_area.py
@@ -1,13 +1,16 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring, missing-final-newline, invalid-name, trailing-newlines, wrong-import-position, unused-import
 import importlib
 import sys
+import matplotlib.pyplot as plt
 import numpy as np
 import shapely
+import shapely.plotting
 sys.path.append("./")
 import kittehs_funkollection as kf
+importlib.reload(plt)
 importlib.reload(np)
 importlib.reload(shapely)
 importlib.reload(kf)
 
-
+cat = kf.eat("input")
 
