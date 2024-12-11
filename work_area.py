ee5578090458ab6de6232442292e07bb904a80a9
KodingForKittehs <grimtalon@gmail.com>
2024-12-10 21:30:25 -0500

    added a few grid related helpers

--- work_area.py
+++ work_area.py
@@ -1,7 +1,7 @@
 # pylint: disable=trailing-whitespace, missing-module-docstring, missing-function-docstring, missing-class-docstring, missing-final-newline, invalid-name, trailing-newlines, wrong-import-position, unused-import
 import functools
 from collections import defaultdict
-from itertools import combinations, permutations
+from itertools import combinations, permutations, product
 import importlib
 import sys
 import igraph
@@ -12,6 +12,10 @@ import shapely.plotting
 sys.path.append("./")
 import kittehs_funkollection as kf
 importlib.reload(kf)
+#np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(linewidth=sys.maxsize)
+np.set_printoptions(edgeitems=10)
 
-grid = kf.to_grid(kf.eat("input"))
 
+
+line = kf.file_as_line("input")
